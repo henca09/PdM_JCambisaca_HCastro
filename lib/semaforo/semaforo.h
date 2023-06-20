d27d88d864fd11256643b62194fd000e5d3ea1ce
JhoCam007 <jhonatanjk936@gmail.com>
2023-06-19 23:23:07 -0500

    Documentación de la cabecera Semaforo.h
    Adición de los comentarios para la módulo de código Semaforo.h

--- lib/semaforo/semaforo.h
+++ lib/semaforo/semaforo.h
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-
+//Variable enum que contienen los posibles estados de la MEF
 typedef enum{
     normal_verde, 
     normal_verde_amarillo,
@@ -9,7 +9,7 @@ typedef enum{
     alarma_rojo, 
     alarma_cero
 }estados;
-
+//Estructura que incluye los Led disponibles y la variable de estado
 typedef struct {
     bool SW1; 
     int16_t LED1;
@@ -17,7 +17,7 @@ typedef struct {
     int16_t LED3;
     estados es; 
 } struct_var;
-
+//Prototipos de funciones
 void inicializar_MEF_semaforo(bool SW1, bool SW2, int16_t LED1, int16_t LED2, int16_t LED3);
 estados actualizar_MEF_semaforo(struct_var var_struct);
 bool tiempo_demora(int16_t delay_1);
